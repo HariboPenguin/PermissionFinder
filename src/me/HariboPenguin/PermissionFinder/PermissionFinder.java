24b00bb6f71ed9473c149773e108608efcc40377
HariboPenguin <HariboPenguin@me.com>
2012-05-26 17:15:44 +0100

    Added in Plugin Metrics

--- src/me/HariboPenguin/PermissionFinder/PermissionFinder.java
+++ src/me/HariboPenguin/PermissionFinder/PermissionFinder.java
@@ -1,21 +1,27 @@
 package me.HariboPenguin.PermissionFinder;
 
+import java.io.IOException;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PermissionFinder extends JavaPlugin {
-    
+
     public FindCommand cmdExecutor = new FindCommand(this);
     public DumpCommand dumpExecutor = new DumpCommand(this);
-    
     public String prefix = ChatColor.DARK_PURPLE + "[" + ChatColor.GOLD + "PermissionFinder" + ChatColor.DARK_PURPLE + "] ";
-    
+
     @Override
     public void onEnable() {
-        
+
+        try {
+            Metrics metrics = new Metrics(this);
+            metrics.start();
+        } catch (IOException e) {
+            // Failed to submit the stats :-(
+        }
+
         getCommand("findperms").setExecutor(cmdExecutor);
         getCommand("dumpperms").setExecutor(dumpExecutor);
-        
+
     }
-    
 }
